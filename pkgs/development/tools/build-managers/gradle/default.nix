77fc280a1b63dfb995d131b4a0fb4054d10b1a1f
Peter A. <ink.splatters@pm.me>
2025-09-06 13:10:51 +0200

    gradle: jdk_x -> temurin-bin-24

--- pkgs/development/tools/build-managers/gradle/default.nix
+++ pkgs/development/tools/build-managers/gradle/default.nix
@@ -1,8 +1,6 @@
 {
   callPackage,
-  jdk17,
-  jdk21,
-  jdk23,
+  temurin-bin-24
 }:
 
 let
@@ -56,7 +54,7 @@ let
           tests = {
             toolchains =
               let
-                javaVersion = lib.getVersion jdk23;
+                javaVersion = lib.getVersion temurin-bin-24;
                 javaMajorVersion = lib.versions.major javaVersion;
               in
               runCommand "detects-toolchains-from-nix-env"
@@ -65,7 +63,7 @@ let
                   nativeBuildInputs = [
                     (gradle.override {
                       javaToolchains = [
-                        jdk23
+			 temurin-bin-24
                       ];
                     })
                   ];
@@ -220,7 +218,7 @@ let
         in
         ''
           # get the correct jar executable for cross
-          export PATH="${buildPackages.jdk}/bin:$PATH"
+          export PATH="${temurin-bin-24}/bin:$PATH"
           . ${./patching.sh}
 
           nativeVersion="$(extractVersion native-platform $out/lib/gradle/lib/native-platform-*.jar)"
@@ -331,19 +329,19 @@ rec {
   gradle_9 = gen' {
     version = "9.1.0";
     hash = "sha256-oX3dhaJran9d23H/iwX8UQTAICxuZHgkKXkMkzaGyAY=";
-    defaultJava = jdk21;
+    defaultJava = temurin-bin-24;
   };
   gradle_8 = gen' {
     version = "8.14.3";
     hash = "sha256-vXEQIhNJMGCVbsIp2Ua+7lcVjb2J0OYrkbyg+ixfNTE=";
-    defaultJava = jdk21;
+    defaultJava = temurin-bin-24;
   };
   gradle_7 = gen' {
     version = "7.6.6";
     hash = "sha256-Zz2XdvMDvHBI/DMp0jLW6/EFGweJO9nRFhb62ahnO+A=";
-    defaultJava = jdk17;
+    defaultJava = temurin-bin-24;
   };
 
   # Default version of Gradle in nixpkgs.
-  gradle = gradle_8;
+  gradle = gradle_9;
 }
